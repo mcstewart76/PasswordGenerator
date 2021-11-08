ed0cccc2de8e360d72bf0c91cca9c29cb49b81b1
mcstewart76 <mcstewart76@gmail.com>
2021-11-08 18:34:09 -0500

    Added initial arrays and testing string values

--- assets/script/script.js
+++ assets/script/script.js
@@ -20,7 +20,12 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   function generatePassword() {
-    return "ActualPass"
+    const low = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const up = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    const num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const char = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\" , "]", "^", "_", "`", "{", "|", "}", "~"]
+    const array1 = low.concat(num).join("");
+    return array1
   }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
