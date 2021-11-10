cf33a7109238068485b5f234046ac86a7a564a40
mcstewart76 <mcstewart76@gmail.com>
2021-11-10 12:44:12 -0500

    Fixed logic using console.log info on each case, added the number of characters prompt

--- assets/script/script.js
+++ assets/script/script.js
@@ -16,40 +16,70 @@
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+ var low = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var up = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var char = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\" , "]", "^", "_", "`", "{", "|", "}", "~"]
+  var array2 = [];
 // Write password to the #password input
 function writePassword() {
-  function generatePassword() {
-    const low = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const up = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    const num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    const char = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\" , "]", "^", "_", "`", "{", "|", "}", "~"]
-    let caseLower = confirm("Do you want lowercase numbers");
-    let caseUpper = confirm("Do you want uppercase numbers");
-    let caseNum = confirm("Do you want numbers?");
-    let caseSpecial = confirm("Do you want special characters");
-
-    if (caseLower) {
-      //const array1 = low.concat(char).join("");
-      const array1 = array1.concat(low).join("");
-      //ask uppercase
-    }
-      if (caseUpper) {
-      const array1 = array1.concat(up).join("");
-      }
-        if (caseNum){
-          const array1 = array1.concat(num).join("");
-        }
-          if (caseSpecial) {
-            const array1 = array1.concat(char).join("");
-          }
-  }
+   
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  
-
 }
+function generatePassword() {
+  NumberOfCharacters = prompt("Enter a number between 8 and 128");
+  if (NumberOfCharacters <=128 && NumberOfCharacters >= 8){
+  console.log("Is between 8 and 128"+ NumberOfCharacters)
+
+  }else{
+    alert("Please enter a value between 8 and 128.")
+    return "Please enter a value between 8 and 128."
+  } 
+  let caseLower = confirm("Do you want lowercase letters");
+  console.log("CaseLower " + caseLower);
+  let caseUpper = confirm("Do you want uppercase letters");
+  console.log("CaseUP " + caseUpper);
+  let caseNum = confirm("Do you want numbers?");
+  console.log("CaseNum " + caseNum);
+  let caseSpecial = confirm("Do you want special characters");
+  console.log("CaseSpec " + caseSpecial);
+  // function amountOfCharacters(charactersnum){
+  //     var charactersnum = prompt("How many characters do you want your password");
+  
+  if (caseLower) {
+    console.log("Inside if CaseLower ");
+    // const array2 = low.concat(char).join("");
+  array2 = array2.concat(low);
+    
+  }
+  console.log(array2)
+    if (caseUpper) {
+      console.log("Inside if CaseUp ");
+    array2 = array2.concat(up).join("");
+    
+    }
+    console.log(array2)
+      if (caseNum){
+        console.log("Inside if CaseNum ");
+        array2 = array2.concat(num).join("");
+        
+      }
+      console.log(array2)
+        if (caseSpecial) {
+          console.log("Inside if CaseSpec ");
+          array2 = array2.concat(char).join("");
+          
+        }  
+        console.log(array2)
+     return array2  
+
+    
+
+} 
+// return array1.join("");
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
